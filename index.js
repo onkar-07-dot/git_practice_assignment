0ade0affc44b13128d60cfa0d83b64d551b7f3c1
[onkar-07-dot] <[onkarnanavare007@gmail.com]>
2025-09-24 03:46:25 +0530

    these are the code of auth

--- index.js
+++ index.js
@@ -1,40 +1,67 @@
 
 const express = require("express")
-const app = express()
+const {connection } = require("./config/db")
+const {UserModel} = require("./models/model")
+const jwt = require("jsonwebtoken")
 
+const app = express()
+app.use(express.json())
 
 app.get("/",(req,res)=>{
-    res.send("this is home page")
+    res.send("this is the home page")
 })
 
+app.post("/register",async(req,res)=>{
+    const payload = req.body
+    try {
+      const user = new UserModel(payload)
+      await user.save()
+        res.send("register sucessfully",user)
+       
+    } catch (error) {
+        res.send("Error in registering user")
+        console.log(error)
+        
+    } 
 
-app.get("/about",(req,res)=>{
-    res.send("this is about page")
+})
+
+app.post("/login",async(req,res)=>{
+    const {name , password} = req.body
+    try {
+        const getuser = await UserModel.find({name , password})
+        const token = jwt.sign({course :"backend"}, "node")
+        if(getuser.length>0){
+            res.send({"message" : "login done" , "token" : token})
+        }else{
+            res.send("wrong credential")
+        }
+        
+    } catch (error) {
+        console.log("something went wrong")
+        
+    }
 })
 
 app.get("/data",(req,res)=>{
-    //res.send("this is about page")
-   
-    const data ={
-        banglore : 23,
-        mumbai : 22,
-        pune : 23,
-    }
-
-    const {city} = req.query
-    res.send(`temprature of the ${city} is the ${data[city]}`)
-
-    
+    const token = req.headers.authorization
+    jwt.verify(token , "node" ,(err,decode)=>{
+        if(err){
+            res.send("invalid token")
+            console.log(err)
+        }else{
+            res.send("data..")
+        }
+    })
 })
 
-  app.get("/student/:roll_no",(req,res)=>{
-
-        const roll_no = req.params.roll_no
-        res.send(`data of students with the roll no ${roll_no}`)
-    })
-
-
-
-app.listen(3000,()=>{
-    console.log("server started successfully")
+app.listen(9090,async()=>{
+    try {
+        await connection
+        console.log("the connected to db")
+        
+    } catch (error) {
+        
+    }
+    console.log("server is started")
 })
