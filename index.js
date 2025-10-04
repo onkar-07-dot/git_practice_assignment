5abb5f54749bd1ec9dd467196f1f06969e074ff3
[onkar-07-dot] <[onkarnanavare007@gmail.com]>
2025-10-05 03:36:13 +0530

    this is the socket.io code

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 
-const {WebSocketServer} = require("ws")
+const express = require("express")
+const {Server} = require("socket.io")
+const app = express()
+const http = require("http")
 
-const wss = new WebSocketServer({port:9000})
+const httpserver = http.createServer(app)
 
-wss.on("connection" , (socket)=>{
-    console.log("got new connection")
-    socket.send("hello from the server")
-   socket.onmessage = (event)=> {console.log(event.data)
-
-   if(event.data == "hello from client"){
-    socket.send("how are you")
-   }}
-    socket.onclose = ()=> console.log("connection disconnected")
+app.get("/",(req,res)=>{
+    res.sendend("this is home page")
 })
+
+app.get("/about",(req,res)=>{
+    res.sendend("this is about page")
+})
+
+
+httpserver.listen(8080)
+const io = new Server(httpserver)
+let count = 0
+let seat = 10
+io.on("connection" , (socket)=>{
+    console.log("client connected")
+    socket.emit("message","welcome to the socket server")
+    socket.emit("greeting","here is your welcome kit")
+    socket.on("hello",(msg)=> console.log(msg))
+    socket.emit("online",count++)
+    socket.broadcast.emit("info","we have new user in the town")
+
+    socket.on("disconnect",()=>{
+        count --;
+        socket.broadcast.emit("online",count)
+    })
+
+    io.emit("announcement",`hey we have only ${seat-count} left`)
+})
+
