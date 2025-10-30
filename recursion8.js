f8031bd6565239bbc92b574261465bba0cd46510
[onkar-07-dot] <[onkarnanavare007@gmail.com]>
2025-10-30 22:54:02 +0530

    this is the recursion code

--- recursion8.js
+++ recursion8.js
@@ -1,20 +1,16 @@
-// rat in a maze
 
 
-function rat(mat,row,col,str){
-    if(row<0 ||row>= mat.length ||col< 0 ||col>=mat.length||mat[row][col]==0){
-        return
+function fn(arr,i , need){
+
+    if(need ==0){
+        return 1
     }
-    if(row == mat.length-1 && col ==mat[0].length-1){
-        console.log(str)
-        return
+    if(need <0 || i<0 ){
+        return 0                     
+
     }
-    mat[row][col] = 0;
-    rat(mat,row-1,col,str+"U");
-    rat(mat,row+1,col,str+"D")
-    rat(mat,row,col-1,str+"L")
-    rat(mat,row,col+1,str+"R")
-    mat[row][col] = 1
+
+    return fn(arr,i-1,need-arr[i]) +fn(arr,i-1,need)
 }
-let matrix = [[1,0,0,0],[1,1,0,1],[1,1,0,0],[0,1,1,1]]
-console.log(rat(matrix,0,0,""))
+
+console.log(fn([1,2,3,4,5],4,5))
