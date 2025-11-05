cf55e5504e6c839ea9e340f45272484ceefc08fb
[onkar-07-dot] <[onkarnanavare007@gmail.com]>
2025-11-06 02:46:01 +0530

    this is the code of sequelize

--- index.js
+++ index.js
@@ -1,91 +1,51 @@
 
 const express = require("express")
-const {Sequelize, DataTypes} = require("sequelize")
+const Sequelize = require("sequelize")
 const app = express()
-
 app.use(express.json())
 
-const sequelize = new Sequelize("students" ,"root","Onkar@2676",{
+const seq = new Sequelize("admission" , "root" , "Onkar@2676",{
 
-    host:"localhost",
+    host : "localhost",
     dialect : "mysql"
+
 })
 
-sequelize.authenticate().then(()=>console.log("connection successfull"))
-.catch(()=>console.log("connection failed"))
+const students = seq.define("students",{
+    name : Sequelize.STRING , 
+    email : Sequelize.STRING,
+    age  : Sequelize.INTEGER
 
-const Users = sequelize.define("users",{
-    id :{
-        type : DataTypes.INTEGER,
-        primaryKey : true,
-        allowNull : false,
+})
 
-    },
-    name:{
-        type : DataTypes.STRING,
-        allowNull : false
-    },
-    email:{
-        type : DataTypes.STRING,
-        allowNull:false,
-        unique : true
+app.post("/api/create",async (req,res)=>{
+
+    const {name , email , age} = req.body
+    try {
+
+        const data = await students.create({
+            name ,
+            email , 
+            age
+        })
+
+        res.send("data created successfully")
+        res.status(200)
+        console.log(data)
+        
+    } catch (error) {
+        console.log(error)
     }
 })
 
-async function fetchdata(){
+app.get("/api/getdata" , async (req,res)=>{
 
-    await sequelize.sync()
-
-    await Users.destroy({
-        where: {
-            id:1
-        }
-    })
-
-    async function insertuser(user){
-        await Users.create(user)
-    }
-
-    app.post("/users",(req,res)=>{
-        const user = req.body
-        insertuser(user)
-        res.send("data is added")
-    })
-    const data = await Users.findAll()
-   console.log(data)
-    .then(()=>{
-
-        Users.bulkCreate([{
-            id : 201,
-            name : "pushkar",
-            email:"push@gmail.com"
-
-        },{
-            id:202,
-            name: "shri",
-            email:"shri@gmail"
-        },
-        {
-            id:203,
-            name : "samu",
-            email : "samu@gmail.com"
-        }
-    ]).then(()=>console.log("data created successfully"))
-    })
-    .then((data)=>console.log(data))
-}
-
-fetchdata()
-sequelize.sync().then(()=>{
-
-    Users.create({id:"100", name:"onkar",email:"onkar@gmail"})
-
-}).then(()=>console.log("user created successfully"))
-.catch(()=>console.log("failed to create data"))
-
-app.listen(5000,()=>{
-    console.log("server started")
+    const data = await students.findAll()
+    res.send(data)
 })
 
-
-
+seq.sync().then(()=>{
+    app.listen(3000,()=>{
+    console.log("the server is started at 3000")
+})
+})
